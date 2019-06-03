c682005b3418dcb040a4ecc441edf9eee843209b
Illia Smolii <illiasmolii@gmail.com>
2019-06-03 09:28:45 +0300

    Initial commit

---
+++ src/test/java/ua/smolii/scaggregator/ScaggregatorApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.smolii.scaggregator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScaggregatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
