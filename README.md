2074601e5eb97a94577561dd2031885622475659
Illia Smolii <illiasmolii@gmail.com>
2019-06-04 09:22:03 +0300

    Access to H2 console.

---
+++ README.md
@@ -0,0 +1,7 @@
+DB Console: 
+
+
+    http://localhost:8080/console/
+    URL: jdbc:h2:mem:testdb
+    username: admin
+    password: password
--- src/main/java/ua/smolii/scaggregator/ScaggregatorApplication.java
+++ src/main/java/ua/smolii/scaggregator/ScaggregatorApplication.java
@@ -1,13 +1,34 @@
 package ua.smolii.scaggregator;
 
+
+import org.h2.server.web.WebServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class ScaggregatorApplication {
+public class ScaggregatorApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScaggregatorApplication.class, args);
 	}
 
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().and()
+				.authorizeRequests().antMatchers("/console/**").permitAll();
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+	}
 }
